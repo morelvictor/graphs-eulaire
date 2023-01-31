1c7b0b16d90903915c60a02ca2b075aaea8e350c
ilian <iliancrague@gmail.com>
2023-01-31 12:43:56 +0100

    surbrillance bouton utilisé

--- src/ui/Editeur.java
+++ src/ui/Editeur.java
@@ -20,10 +20,14 @@ public class Editeur extends JPanel {
 	//indice dans la liste sommets du sommet à lier
 	private int a_lier = -1;
 
+	private JButton suppr_som;
+	private boolean peut_suppr = false;
+
+	private JButton suppr_all;
+
 	private JButton generer_random;
 	private static Random r = new Random();
 
-	private JButton suppr_all;
 
 	public Editeur() {
 
@@ -32,7 +36,6 @@ public class Editeur extends JPanel {
 		add(vuegraphe);
 
 		poser_sommet = new JButton("o");
-
 		poser_sommet.setBounds(900, 50, 50, 50);
 		add(poser_sommet);
 
@@ -40,7 +43,10 @@ public class Editeur extends JPanel {
 			(ActionEvent e) -> {
 			peut_poser_sommet = !peut_poser_sommet;
 			peut_lier = false;
+			peut_suppr = false;
+			repaint();
 		});
+
 
 		lier = new JButton("x");
 		lier.setBounds(900, 110, 50, 50);
@@ -50,11 +56,39 @@ public class Editeur extends JPanel {
 			(ActionEvent e) -> {
 			peut_lier = !peut_lier;
 			peut_poser_sommet = false;
+			peut_suppr = false;
 			a_lier = -1;
+			repaint();
 		});
 
+
+		suppr_som = new JButton("☒");
+		suppr_som.setBounds(900, 170, 50, 50);
+		add(suppr_som);
+
+		suppr_som.addActionListener(
+			(ActionEvent e) -> {
+			peut_suppr = !peut_suppr;
+			peut_poser_sommet = false;
+			peut_lier = false;
+			repaint();
+			});
+
+
+		suppr_all = new JButton("⟲");
+		suppr_all.setBounds(900, 230, 50, 50);
+		add(suppr_all);
+
+		suppr_all.addActionListener(
+			(ActionEvent e) -> {
+			vuegraphe.viderGraphe();
+			repaint();
+		});
+		repaint();
+
+
 		generer_random = new JButton("?");
-		generer_random.setBounds(900, 170, 50, 50);
+		generer_random.setBounds(900, 290, 50, 50);
 		add(generer_random);
 
 
@@ -66,16 +100,6 @@ public class Editeur extends JPanel {
 			repaint();
 		});
 
-		suppr_all = new JButton("☒");
-		suppr_all.setBounds(900, 230, 50, 50);
-		add(suppr_all);
-
-		suppr_all.addActionListener(
-			(ActionEvent e) -> {
-			vuegraphe.viderGraphe();
-			repaint();
-		});
-		repaint();
 	}
 
 	public static LinkedList<Point> getNRandomCoord(int n) {
@@ -119,10 +143,27 @@ public class Editeur extends JPanel {
 	}
 
 	public void paintComponent(Graphics g) {
-		lier.setBounds(getWidth() - 100, 110, 50, 50);
+		g.setColor(COULEUR);
+		if(peut_poser_sommet){
+			g.setColor(Color.green);
+		}
+		((Graphics2D) g).draw(new Rectangle(getWidth() - 101, 49, 51, 51));
+		g.setColor(COULEUR);
+		if(peut_lier){
+			g.setColor(Color.green);
+		}
+		((Graphics2D) g).draw(new Rectangle(getWidth() - 101, 109, 51, 51));
+		g.setColor(COULEUR);
+		if(peut_suppr){
+			g.setColor(Color.green);
+		}
+		((Graphics2D) g).draw(new Rectangle(getWidth() - 101, 169, 51, 51));
+		g.setColor(COULEUR);
 		poser_sommet.setBounds(getWidth() - 100, 50, 50, 50);
-		generer_random.setBounds(getWidth() - 100, 170, 50, 50);
+		lier.setBounds(getWidth() - 100, 110, 50, 50);
+		suppr_som.setBounds(getWidth() - 100, 170, 50, 50);
 		suppr_all.setBounds(getWidth() - 100, 230, 50, 50);
+		generer_random.setBounds(getWidth() - 100, 290, 50, 50);
 	}
 
 
