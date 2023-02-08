7d8f0995d1e03c96b2bc97f1535e22b3a128b896
Victor <victor.morel@etu.u-paris.fr>
2023-02-08 12:07:01 +0100

    Vue partie cree

---
+++ src/ui/Partie.java
@@ -0,0 +1,21 @@
+
+import java.awt.*;
+import java.awt.event.*;
+import java.awt.geom.Ellipse2D;
+import java.util.*;
+import javax.swing.*;
+
+public class Partie extends VueGraphe {
+	public Partie() {
+		super(Color.black, 30, new MouseListener() {
+	}
+			public void mouseExited(MouseEvent e) {}
+			public void mouseEntered(MouseEvent e) {}
+			public void mouseReleased(MouseEvent e) {}
+			public void mousePressed(MouseEvent e) {}
+			public void mouseClicked(MouseEvent e) {
+				
+			}
+		});
+	}
+}
