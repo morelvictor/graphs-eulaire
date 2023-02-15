2f1646c3d9b90e8769804ec28a49c2e9b75cb736
Amelia Coutard <eliottulio.coutard@gmail.com>
2023-02-15 15:34:44 +0100

    Fixed formatting, added a script to autoomatically format all java files

--- src/ui/Editeur.java
+++ src/ui/Editeur.java
@@ -427,16 +427,11 @@ public class Editeur extends JPanel {
 			repaint();
 		}
 
-		public void mouseEntered(MouseEvent e) {
-		}
-		public void mouseExited(MouseEvent e) {
-		}
-		public void mousePressed(MouseEvent e) {
-		}
-		public void mouseReleased(MouseEvent e) {
-		}
-		public void mouseDragged(MouseEvent e) {
-		}
+		public void mouseEntered(MouseEvent e) {}
+		public void mouseExited(MouseEvent e) {}
+		public void mousePressed(MouseEvent e) {}
+		public void mouseReleased(MouseEvent e) {}
+		public void mouseDragged(MouseEvent e) {}
 		public void mouseMoved(MouseEvent e) {
 			if (getADeplacer() != -1) {
 				vuegraphe.setCoordonnees(a_deplacer, e.getX() - DIAMETRE / 2, e.getY() - DIAMETRE / 2);
